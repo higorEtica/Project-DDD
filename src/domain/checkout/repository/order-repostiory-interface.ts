dfd12d545c96bcd70bfaaf73dc6d005df76a6fda
HigorLucas_Etica <higor_etica@algartelecom.com.br>
2025-04-17 14:14:25 -0300

    Modulos atualizados

--- src/domain/checkout/repository/order-repostiory-interface.ts
+++ src/domain/checkout/repository/order-repostiory-interface.ts
@@ -1,6 +1,7 @@
 
+
+import RepositoryInterface from "../../repository/repository-inteface";
 import Order from "../entity/order";
-import RepositoryInterface from "../repository/repository-inteface";
 
 
 export default interface OrderRepositoryInterface  extends RepositoryInterface<Order> {}
